81924b79a11718565280062821a1274849481109
rcanfield <Rcanfield86@gmail.com>
2024-01-05 23:48:10 -0500

    Remove old code

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import { CodeSuggestionProvider } from "./providers/codeSuggestionProvider.js";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext, service: vscode.Theme) {
+export async function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "wing-man" is now active!');
@@ -32,7 +32,6 @@ export async function activate(context: vscode.ExtensionContext, service: vscode
 	// 	)
 	// )
 
-	vscode.window.reg
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
