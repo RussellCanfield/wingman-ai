9a6e05d943656b01d771fdb29988c71ae1221c77
rcanfield <Rcanfield86@gmail.com>
2024-09-30 10:30:00 -0400

    Auto complete prompt tweak

--- src/service/openai/models/gptmodel.ts
+++ src/service/openai/models/gptmodel.ts
@@ -10,6 +10,7 @@ export class GPTModel implements OpenAIModel {
 		return `Fill in the following text.
 Do not include the original text in your response, just the middle portion.
 Return your response in plain text, do not use a markdown format.
+If the code provided does not provide a clear intent and you are unable to complete the code, respond with an empty string "".
 
 {beginning} <FILL_HOLE> {ending}`;
 	}
