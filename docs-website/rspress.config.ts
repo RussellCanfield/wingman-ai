7bd6b9e9131a105f8240158d553066646fac0178
Russell Canfield <rcanfield86@gmail.com>
2026-02-02 11:28:47 -0500

    Add new open graph image

--- docs-website/rspress.config.ts
+++ docs-website/rspress.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'node:path';
 import { defineConfig } from '@rspress/core';
+import { pluginOpenGraph } from 'rsbuild-plugin-open-graph';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -10,6 +11,20 @@ export default defineConfig({
     light: '/wingman_logo.webp',
     dark: '/wingman_logo.webp',
   },
+  plugins: [
+      pluginOpenGraph({
+        title: 'Wingman AI Home Page',
+        siteName: 'Wingman AI Docs',
+        type: 'website',
+        url: 'https://getwingmanai.com/',
+        image: 'https://getwingmanai.com/wingman_opengraph.webp',
+        description: 'Multi-agent orchestration for local and distributed coding teams.',
+        twitter: {
+          site: '@russellcanfield',
+          card: 'summary_large_image',
+        },
+    }),
+  ],
   globalStyles: path.join(__dirname, 'styles', 'global.css'),
   themeConfig: {
     socialLinks: [
